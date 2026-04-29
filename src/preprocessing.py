5656e54f13025210901394e87875ff732267d044
ryamamoto-create <rizyamamoto@gmail.com>
2026-04-29 14:25:07 -0400

    created train-test split in preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -6,9 +6,15 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import numpy as np
 import pandas as pd
-from config import SEED, RATING_PARQUET_PATH
+from config import SEED
 
-df = pd.read_parquet(RATING_PARQUET_PATH)
-test_df = df.groupby("movie_id").sample(n=1, random_state=SEED)
-train_df = df.drop(test_df.index)
+def train_test_split(df, random_state=SEED):
+    # Group by movie_id and sample one rating per movie for the test set
+    test_df = df.groupby("movie_id").sample(n=1, random_state=random_state)
+    
+    # The training set is the original dataframe minus the test set
+    train_df = df.drop(test_df.index)
+    
+    return train_df, test_df
+
 
